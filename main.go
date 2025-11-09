f56ee8557e4b7ff7e491805e915a76493859d808
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-11-09 20:33:14 +0900

    Implement simple hello world server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type RespHandler struct{}
+
+func (h *RespHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("method: %s\n", r.Method)
+	log.Printf("url:    %s\n", r.URL)
+	fmt.Fprint(w, "Hello World")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	handler := RespHandler{}
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: &handler,
+	}
+
+	log.Println("Server Listen at :8080 port...")
+	server.ListenAndServe()
 }
